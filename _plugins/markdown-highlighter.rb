d26090da31ca34e77ffb799b0da36bb6e34f5100
raultizze <rtizze@gmail.com>
2023-06-21 00:48:26 -0300

    sin highlighter

--- _plugins/markdown-highlighter.rb
+++ _plugins/markdown-highlighter.rb
@@ -2,17 +2,17 @@
 
 # Turns ==something== in Markdown to <mark>something</mark> in output HTML
 
-Jekyll::Hooks.register [:notes], :pre_render do |doc|
-  replace(doc)
-end
+# Jekyll::Hooks.register [:notes], :pre_render do |doc|
+#   replace(doc)
+# end
 
-Jekyll::Hooks.register [:pages], :pre_render do |doc|
-  # jekyll considers anything at the root as a page,
-  # we only want to consider actual pages
-  next unless doc.path.start_with?('_pages/')
-  replace(doc)
-end
+# Jekyll::Hooks.register [:pages], :pre_render do |doc|
+#   # jekyll considers anything at the root as a page,
+#   # we only want to consider actual pages
+#   next unless doc.path.start_with?('_pages/')
+#   replace(doc)
+# end
 
-def replace(doc)
-  doc.content.gsub!(/==+([^ ](.*?)?[^ .=])==+/, '<mark class="centered">\1</mark>')
-end
+# def replace(doc)
+#   doc.content.gsub!(/==+([^ ](.*?)?[^ .=])==+/, '<mark class="centered">\1</mark>')
+# end
